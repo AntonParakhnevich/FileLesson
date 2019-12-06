be3b241b68f09110140959308d9e51f9e7c41527
AntonParakhnevich <an_tony9@mail.ru>
2019-12-06 09:52:45 +0300

    commit 3

--- src/Letter.java
+++ src/Letter.java
@@ -1,34 +1,58 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.*;
 
+import static java.util.Collections.sort;
+
 public class Letter {
-    private static final String b="абвгдеёжзийклмнопрcтухцчшщъыьэюя";
+    private static final String alphabet="абвгдеёжзийклмнопрcтухцчшщъыьэюя";
+    private static final File text=new File("Pushkin");
+
     public static void main(String[] args) {
-        ArrayList<String> element = new ArrayList<>();
-        try (Scanner scanner=new Scanner(new BufferedReader(new FileReader("Pushkin")))){
-            while (scanner.hasNext()){
-             String[]   a=scanner.next().split("");
-                for (int i = 0; i <a.length ; i++) {
-                    String e=a[i].toLowerCase();
-                    if(b.contains(e)) {
-                        element.addAll(Collections.singletonList(e));
-                    }
+        File file= new File("letter.txt");
+        List<String> letters = element();
+        writeToFile(quantityLetter(letters),file);
+    }
+
+    private static List<String> quantityLetter(List<String> list){
+
+            List<String> letterUnique= new ArrayList<>();
+            List<String> letter = new ArrayList<>(new HashSet<>(list));
+        for (String item : letter) {
+            int s = 0;
+            for (String value : list) {
+                if (item.equals(value)) {
+                    s++;
                 }
             }
-            ArrayList<String> letterUnique = new ArrayList<>(new HashSet<>(element));
-            System.out.println(letterUnique);
-            for (int i = 0; i <letterUnique.size() ; i++) {
-                int s=0;
-                for (String value: element) {
-                    if (letterUnique.get(i).equals(value)) {
-                        s++;
+            letterUnique.add(item + " " + s);
+        }
+        return letterUnique;
+    }
+
+    private static List<String> element(){
+        List<String> letters = new ArrayList<>();
+        try (Scanner scanner=new Scanner(new BufferedReader(new FileReader(Letter.text)))){
+            while (scanner.hasNext()){
+                String[]   word=scanner.next().split("");
+                for (String s : word) {
+                    String letter = s.toLowerCase();
+                    if (alphabet.contains(letter)) {
+                        letters.addAll(Collections.singleton(letter));
                     }
                 }
-                    System.out.println(letterUnique.get(i) + " " + s);
             }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return letters;
+    }
+
+    private static void writeToFile(List<String> list, File file){
+        try(PrintWriter printWriter=new PrintWriter(new BufferedWriter(new FileWriter(file)))){
+            for (String s : list) {
+                printWriter.write(s + " " + "\n");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
